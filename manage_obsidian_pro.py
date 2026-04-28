8a462073c94870b1c08859a2c4a3c12ac82ddf11
Virgil Pro <virgil.mesle@sirius-space.fr>
2026-04-28 14:14:50 +0200

    wip

--- manage_obsidian_pro.py
+++ manage_obsidian_pro.py
@@ -3,25 +3,30 @@ from utils_obsidian import *
 OBS_PRO = r"C:\Users\virgil.mesle\OneDrive - Sirius Space Services\Documents\obsidian_pro"
 OBS_LOS = r"C:\Users\virgil.mesle\OneDrive - Sirius Space Services\Documents\LOS\obsidian_los"
 
-move_md_by_yaml_header(
-    root_dir=OBS_PRO,
-    yaml_key="tags",
-    yaml_value="companies",
-    dest_dir=os.path.join(OBS_PRO, "companies")
-)
 
-move_md_by_yaml_header(
-    root_dir=OBS_LOS,
-    yaml_key="type",
-    yaml_value="Document",
-    dest_dir=os.path.join(OBS_LOS, "Documents")
-)
+def main():
+    move_md_by_yaml_header(
+        root_dir=OBS_PRO,
+        yaml_key="tags",
+        yaml_value="companies",
+        dest_dir=os.path.join(OBS_PRO, "companies")
+    )
 
-move_md_by_yaml_header(
-    root_dir=OBS_LOS,
-    yaml_key="type",
-    yaml_value="Spec",
-    dest_dir=os.path.join(OBS_LOS, "Specs")
-)
+    move_md_by_yaml_header(
+        root_dir=OBS_LOS,
+        yaml_key="type",
+        yaml_value="Document",
+        dest_dir=os.path.join(OBS_LOS, "Documents")
+    )
 
-process_vault(OBS_PRO)
+    move_md_by_yaml_header(
+        root_dir=OBS_LOS,
+        yaml_key="type",
+        yaml_value="Spec",
+        dest_dir=os.path.join(OBS_LOS, "Specs")
+    )
+
+    process_vault(OBS_PRO)
+
+if __name__ == "__main__":
+    main()
